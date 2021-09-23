f068839b53b34c41322dd8d1637831ff0b808694
omneex <dtc97bh@gmail.com>
2021-09-23 14:37:44 -0600

    Add redis sessions

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,67 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import helmet from 'helmet';
-import { indexRoute } from './routes';
-import { logger, middlewareLogger } from './logging';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import crypto from 'crypto';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import Redis from 'ioredis';
+import { logger, middlewareLogger } from './logging';
+import { indexRoute } from './routes';
 import { testdbroute } from './routes/test/db';
 
 dotenv.config();
 const app = express();
+const redisClient = new Redis(6379, 'redis');
+redisClient.on('error', (errorObj) => {
+  const errorString = JSON.stringify(errorObj, null, 2);
+  if (errorObj.code === 'ENOTFOUND') {
+    logger.error('Could not connect to Redis, check that it is running and the URL is correct.');
+  } else {
+    logger.error(errorString);
+  }
+});
+redisClient.on('ready', () => {
+  logger.info('Connected to Redis!');
+});
 
+const RedisStore = connectRedis(session);
 async function prestart() {
-    // Connect to mongoose before continuing, if its not set then log the error and exit
-    if (process.env.MONGODB_URI) {
-        try {
-            await mongoose.connect(process.env.MONGODB_URI);
-            logger.info('Connected to MongoDB');
-        } catch (e) {
-            logger.error('Could not connect');
-        }
-    } else {
-        logger.error('MongoDB connection string was not set!');
-        process.exit(1);
+  // Connect to mongoose before continuing, if its not set then log the error and exit
+  if (process.env.MONGODB_URI) {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI);
+      logger.info('Connected to MongoDB');
+    } catch (e) {
+      logger.error('Could not connect');
     }
+  } else {
+    logger.error('MongoDB connection string was not set!');
+    process.exit(1);
+  }
 }
 
 // This is just a hacky way of avoiding using async/await syntax at top-level
 prestart().catch((e) => {
-    logger.error('Error occurred during prestart!');
+  logger.error('Error occurred during prestart!');
 });
 
 app.use(middlewareLogger); // for logging internal express logging
 app.use(helmet()); // security basics
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    // @ts-ignore redis client will always be initialized before this.
+    store: new RedisStore({ client: redisClient }),
+    saveUninitialized: false,
+    secret:
+            process.env.SESSION_SECRET
+            || crypto.randomBytes(64).toString('hex'),
+    resave: false,
+  }),
+);
 
 app.use('/', indexRoute);
 app.use('/test', testdbroute);
