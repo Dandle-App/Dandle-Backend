328d22a383f200ab05fd5b87df7174244a0ebe2f
Naji Shamas <najishamas100@protonmail.com>
2021-12-06 21:47:34 -0600

    progress on request/info route

--- src/routes/requests/info.ts
+++ src/routes/requests/info.ts
@@ -1,36 +1,47 @@
 import express, {Request, Response} from 'express';
 import { io } from "socket.io-client";
 import { logger } from '../../logging';
-import { param, validationResult } from 'express-validator';
+import { param, validationResult, query } from 'express-validator';
 import parser from 'body-parser';
 import connectRedis from 'connect-redis';
+import ioredis from 'ioredis';
 import redisClient from '../../redis';
 
 const router = express.Router();
-
 interface InfoRequest extends Request {
-    id: string;
+    id?: string;
 }
 const requestInfoRouter = router.get('/',
     [
-         param('id').isString().withMessage('id must be a string'),
+         query('id').isString().withMessage('id must be a string'),
     ],
-    async (req: any, res: Response) => {
-        // check if request has errors
-        JSON.stringify(req.body);
-        logger.info(req.body);
+    async (req: InfoRequest, res: Response) => {
+        // check if request has error
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             logger.error(errors.array());
             return res.status(400).json({ errors: errors.array() });
         }
-    },
-    async (_req: any, res: Response) => {
-        logger.info('Requesting info');
-        // redis connect stuff here
-        res.status(200).send({
-            message: 'Hello, world!',
-        });
+        else {
+            logger.info('Requesting info');
+            logger.info(req.query.id);
+            let queryId = JSON.stringify(req.query.id);
+            redisClient.get(queryId, (err, reply) => {
+                if(err){
+                    res.status(401).json({err: err});
+                    return;
+                }
+                else{
+                    logger.info(reply);
+                    // @ts-ignore
+                    let replyStr = reply;
+                    res.status(200).send({
+                        message: replyStr,
+                    });
+                }
+
+            });
+        }
     });
 
 export default requestInfoRouter;
